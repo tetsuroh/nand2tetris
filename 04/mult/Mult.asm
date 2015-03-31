b58fd7f70a85effef212049a0de22594a304135f
Tetsuroh <tetsuroh.js@gmail.com>
2015-03-31 19:17:31 +0900

    Implement Mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,45 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+        
+        @R2
+        M=0
+        @cnt
+        M=1
+        @R0
+        D=M
+        @R1
+        D=D-M
+        @LOOP
+        0;JGT
+(SWAP)
+        @R0
+        D=M
+        @R3
+        M=D
+        @R1
+        D=M
+        @R0
+        M=D
+        @R3
+        D=M
+        @R1
+        M=D
+(LOOP)
+        @cnt
+        D=M
+        @R1
+        D=D-M
+        @END
+        D;JGT
+        @R0
+        D=M
+        @R2
+        M=M+D
+        @cnt
+        M=M+1
+        @LOOP
+        0;JMP
+(END)
+        @END
+        0;JMP
