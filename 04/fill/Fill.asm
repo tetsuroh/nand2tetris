8c50176418256f343cefdb3315d05511ac2e204a
Tetsuroh <tetsuroh.js@gmail.com>
2015-03-31 19:17:49 +0900

    Impelement Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -8,4 +8,45 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+(RESET)        
+        @cnt
+        M=0
+        @SCREEN
+        D=A
+        @screen
+        M=D
+(LOOP)
+        @color
+        M=0
+        @KBD
+        D=M // any key does not pressed
+        @FILL
+        D;JEQ
+
+        // any key pressed
+        @color
+        M=-1
+        
+(FILL)
+        @cnt
+        D=M
+        @8192 // SCREEN data length
+        D=D-A
+        @RESET
+        D;JEQ
+
+        @color
+        D=M
+        @screen
+        A=M
+        M=D
+
+        @cnt
+        M=M+1
+        @screen
+        M=M+1
+        
+        @LOOP
+        0;JMP
+        
+        
