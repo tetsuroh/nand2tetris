d6948c3947bc625a8e6c3db4b3b2a95809669b86
Tetsuroh <tetsuroh.js@gmail.com>
2015-12-12 16:27:12 +0900

    Modify Main.hs

--- vmtranslator/src/Main.hs
+++ vmtranslator/src/Main.hs
@@ -1,7 +1,10 @@
 module Main where
 
 import System.Environment (getArgs)
-import System.FilePath (takeFileName, dropExtensions, replaceFileName)
+import System.FilePath (takeDirectory, replaceFileName, takeBaseName, splitDirectories, (</>))
+import System.Directory (doesFileExist, getDirectoryContents, setCurrentDirectory, getCurrentDirectory)
+
+import Data.List (isSuffixOf)
 
 import Language.Hack.VM.Compiler (compile)
 import Language.Hack.VM.Parser (parseHackVM)
@@ -9,12 +12,22 @@ import Language.Hack.VM.Parser (parseHackVM)
 main :: IO ()
 main = do
   [file] <- getArgs
-  s <- readFile file
-  let filename = dropExtensions . takeFileName $ file
-      out = replaceFileName file $ filename ++ ".asm"
-      vml = parseHackVM "Hack VM" s
-  case vml of
-    Right v  -> do
-             let asm = compile v filename
+  let directory = (++"/") . takeDirectory $ file
+  isFile <- doesFileExist file
+  files <- if isFile then
+               return [file]
+           else
+               fmap (filter (".vm"`isSuffixOf`)) $
+                    getDirectoryContents directory
+  setCurrentDirectory directory
+  currentDirectory <- getCurrentDirectory
+  sources <- mapM readFile files
+  print files
+  let vmls = mapM (parseHackVM "Hack VM") sources
+      baseNames = map takeBaseName files
+      out = currentDirectory </> last (splitDirectories currentDirectory) ++ ".asm"
+  case vmls of
+    Right vs  -> do
+             let asm = compile $ zip vs baseNames
              writeFile out asm
-    Left err -> print err
+    Left errs -> print errs
